a32009c623b79784ac3d03b450d0bef11b749bbf
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-08-30 11:20:43 -0700

    create function toggleReadStatus function to toggle the reading status of the book

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const author = document.querySelector('#author')
 const pages = document.querySelector('#pages')
 let read = document.getElementById('read')
 let notRead = document.getElementById('notRead')
+let readStatus;
 
 //list of book objects
 let myLibrary = [{
@@ -15,6 +16,12 @@ let myLibrary = [{
   },
   ];
 
+  //logic to toggle read status of the book
+function toggleReadStatus(index) {
+    myLibrary[index].read = myLibrary[index].read === 'read' ? 'not read' : 'read';
+    render();
+  }
+
   //book object constructor
 class Book {
     constructor(name, author, pages, read) {
@@ -24,6 +31,9 @@ class Book {
       this.read = read;
     }
   }
+
+// Add the event listener to the button
+button.addEventListener("click", addBookToLibrary);
 
 // create newbook object and push to the myLibrary
 function addBookToLibrary() {
@@ -37,6 +47,7 @@ function addBookToLibrary() {
     nameBook.value ="";
     author.value = "";
     pages.value = ""; 
+    render()  //render and show the added book to the page
 }
 
 //to render and show the book objects from myLibrary list
@@ -52,6 +63,16 @@ function render(){
     <li class='readStatus'><span class="read-status" data-index="${index}">${item.read}</span></li>
     <li><button class="delete-btn" data-name="${item.name}">Delete</button></li>`;
     card.appendChild(listItem);
+
+})
+    
+   //event listener to toogle the read status of the book using toogleReadStatus function
+    const readStatusSpans = document.querySelectorAll('.read-status');
+    readStatusSpans.forEach((readStatusSpan) => {
+      readStatusSpan.addEventListener('click', (event) => {
+        const indexToToggle = parseInt(event.target.getAttribute('data-index'));
+        toggleReadStatus(indexToToggle);
+      });
 })
 }
 render()
